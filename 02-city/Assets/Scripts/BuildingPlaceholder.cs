2546865218b7fb718fb3deda8096a7c2ff9498b4
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2022-02-26 16:30:00 +0700

    feat(city): full basic gameplay completed

--- 02-city/Assets/Scripts/BuildingPlaceholder.cs
+++ 02-city/Assets/Scripts/BuildingPlaceholder.cs
@@ -1,32 +1,108 @@
 using System;
 using UnityEngine;
 
+public class Cost
+{
+    public int food;
+    public int people;
+    public int energy;
+    public int money;
+
+    public Cost(int food, int people, int energy, int money)
+    {
+        this.food = food;
+        this.people = people;
+        this.energy = energy;
+        this.money = money;
+    }
+}
+
 public class BuildingPlaceholder : MonoBehaviour
 {
     public GameObject smallFarmPrefab;
     public GameObject bigFarmPrefab;
+    public GameObject smallHousePrefab;
+    public GameObject bigHousePrefab;
+    public GameObject smallPowerPlantPrefab;
+    public GameObject bigPowerPlantPrefab;
+    public GameObject smallBusinessPrefab;
+    public GameObject bigBusinessPrefab;
 
     private (int, int) smallFarmSpace = (2, 1);
     private (int, int) bigFarmSpace = (2, 2);
+    private (int, int) smallHouseSpace = (1, 1);
+    private (int, int) bigHouseSpace = (2, 2);
+    private (int, int) smallPowerPlantSpace = (1, 1);
+    private (int, int) bigPowerPlantSpace = (2, 2);
+    private (int, int) smallBusinessSpace = (1, 1);
+    private (int, int) bigBusinessSpace = (2, 2);
+
+    private Cost smallFarmCost = new Cost(0, 1, 0, 0);
+    private Cost bigFarmCost = new Cost(0, 3, 3, 0);
+    private Cost smallHouseCost = new Cost(3, 0, 0, 0);
+    private Cost bigHouseCost = new Cost(7, 0, 2, 0);
+    private Cost smallPowerPlantCost = new Cost(0, 10, 0, 0);
+    private Cost bigPowerPlantCost = new Cost(0, 30, 0, 0);
+    private Cost smallBusinessCost = new Cost(4, 4, 4, 0);
+    private Cost bigBusinessCost = new Cost(10, 10, 10, 0);
 
     private GameObject placeholderGameObject = null;
 
-    private GameObject GetPrefabFromKey(string key) {
-        return Global.buildingBeingPlaced == "small-farm" ? smallFarmPrefab : bigFarmPrefab;
+    private GameObject GetPrefabFromKey(string key)
+    {
+        return Global.buildingBeingPlaced == "small-farm"
+            ? smallFarmPrefab
+            : Global.buildingBeingPlaced == "big-farm"
+            ? bigFarmPrefab
+            : Global.buildingBeingPlaced == "small-house"
+            ? smallHousePrefab
+            : Global.buildingBeingPlaced == "big-house"
+            ? bigHousePrefab
+            : Global.buildingBeingPlaced == "small-power-plant"
+            ? smallPowerPlantPrefab
+            : Global.buildingBeingPlaced == "big-power-plant"
+            ? bigPowerPlantPrefab
+            : Global.buildingBeingPlaced == "small-business"
+            ? smallBusinessPrefab
+            : Global.buildingBeingPlaced == "big-business"
+            ? bigBusinessPrefab
+            : null;
     }
 
-    private (int, int) GetSpaceFromKey(string key) {
-        return Global.buildingBeingPlaced == "small-farm" ? smallFarmSpace : Global.buildingBeingPlaced == "big-farm" ? bigFarmSpace : (0, 0);
+    private (int, int) GetSpaceFromKey(string key)
+    {
+        return Global.buildingBeingPlaced == "small-farm"
+            ? smallFarmSpace
+            : Global.buildingBeingPlaced == "big-farm"
+            ? bigFarmSpace
+            : Global.buildingBeingPlaced == "small-house"
+            ? smallHouseSpace
+            : Global.buildingBeingPlaced == "big-house"
+            ? bigHouseSpace
+            : Global.buildingBeingPlaced == "small-power-plant"
+            ? smallPowerPlantSpace
+            : Global.buildingBeingPlaced == "big-power-plant"
+            ? bigPowerPlantSpace
+            : Global.buildingBeingPlaced == "small-business"
+            ? smallBusinessSpace
+            : Global.buildingBeingPlaced == "big-business"
+            ? bigBusinessSpace
+            : (0, 0);
     }
 
-    private bool HasEnoughSpace((int, int) coords, (int, int) buildingSpace) {
-        for (int i = 0; i < buildingSpace.Item1; i++) {
-            for (int j = 0; j < buildingSpace.Item2; j++) {
+    private bool HasEnoughSpace((int, int) coords, (int, int) buildingSpace)
+    {
+        for (int i = 0; i < buildingSpace.Item1; i++)
+        {
+            for (int j = 0; j < buildingSpace.Item2; j++)
+            {
                 // Out of map bounds
-                if (coords.Item1 + i >= Map.grid.GetLength(0) || coords.Item2 + j >= Map.grid.GetLength(1)) {
+                if (coords.Item1 + i >= Map.grid.GetLength(0) || coords.Item2 + j >= Map.grid.GetLength(1))
+                {
                     return false;
                 }
-                if (Map.grid[coords.Item1 + i, coords.Item2 + j]) {
+                if (Map.grid[coords.Item1 + i, coords.Item2 + j])
+                {
                     return false;
                 }
             }
@@ -34,7 +110,8 @@ public class BuildingPlaceholder : MonoBehaviour
         return true;
     }
 
-    private void OccupySpace((int, int) coords, (int, int) buildingSpace) {
+    private void OccupySpace((int, int) coords, (int, int) buildingSpace)
+    {
         for (int i = 0; i < buildingSpace.Item1; i++)
         {
             for (int j = 0; j < buildingSpace.Item2; j++)
@@ -44,7 +121,8 @@ public class BuildingPlaceholder : MonoBehaviour
         }
     }
 
-    private Vector3? GetMouseWorldPosition() {
+    private Vector3? GetMouseWorldPosition()
+    {
         Plane plane = new Plane(Vector3.up, 0);
         float distance;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -55,9 +133,11 @@ public class BuildingPlaceholder : MonoBehaviour
         return null;
     }
 
-    private (int, int) GetMouseCellCoords() {
+    private (int, int) GetMouseCellCoords()
+    {
         Vector3? worldPosMaybe = GetMouseWorldPosition();
-        if (worldPosMaybe != null) {
+        if (worldPosMaybe != null)
+        {
             Vector3 worldPos = (Vector3)worldPosMaybe;
             float step = 1f;
             (int, int) coords = (
@@ -68,29 +148,64 @@ public class BuildingPlaceholder : MonoBehaviour
         return (0, 0);
     }
 
-    private bool CanBuildHere((int, int) mouseCellCoords) {
+    private bool CanBuildHere((int, int) mouseCellCoords)
+    {
         if (
             mouseCellCoords.Item1 >= 0 && mouseCellCoords.Item1 < Map.grid.GetLength(0) &&
             mouseCellCoords.Item2 >= 0 && mouseCellCoords.Item2 < Map.grid.GetLength(1)
-            ) {
+            )
+        {
             return HasEnoughSpace(mouseCellCoords, GetSpaceFromKey(Global.buildingBeingPlaced));
         }
         return false;
     }
 
+    private bool HasResources(Cost cost)
+    {
+        if (cost == null)
+        {
+            throw new Exception("Cannot find cost");
+        }
+
+        if (Global.food < cost.food)
+        {
+            return false;
+        }
+
+        if (Global.people < cost.people)
+        {
+            return false;
+        }
+
+        if (Global.energy < cost.energy)
+        {
+            return false;
+        }
+
+        if (Global.money < cost.money)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
     void Update()
     {
-        if (Input.GetButtonDown("Cancel")) {
-            Global.buildingBeingPlaced = "";
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Global.buildingBeingPlaced = null;
             transform.position = new Vector3(-30f, 0, 0);
         }
 
         // Should be event
-        if (Global.buildingBeingPlaced != null && placeholderGameObject == null) {
+        if (Global.buildingBeingPlaced != null && placeholderGameObject == null)
+        {
             placeholderGameObject = Instantiate(GetPrefabFromKey(Global.buildingBeingPlaced), new Vector3(0, 0, 0), Quaternion.identity);
         }
         // Should be event
-        if (Global.buildingBeingPlaced == null && placeholderGameObject != null) {
+        if (Global.buildingBeingPlaced == null && placeholderGameObject != null)
+        {
             Destroy(placeholderGameObject);
         }
 
@@ -102,14 +217,50 @@ public class BuildingPlaceholder : MonoBehaviour
                 Vector3 pos = new Vector3(worldPosMaybe.Item1 + 1f, 0.1f, worldPosMaybe.Item2);
                 placeholderGameObject.transform.position = pos;
 
-                if (Input.GetMouseButtonDown(0)) {
+                Cost cost = Global.buildingBeingPlaced == "small-farm"
+                    ? smallFarmCost
+                    : Global.buildingBeingPlaced == "big-farm"
+                    ? bigFarmCost
+                    : Global.buildingBeingPlaced == "small-house"
+                    ? smallHouseCost
+                    : Global.buildingBeingPlaced == "big-house"
+                    ? bigHouseCost
+                    : Global.buildingBeingPlaced == "small-power-plant"
+                    ? smallPowerPlantCost
+                    : Global.buildingBeingPlaced == "big-power-plant"
+                    ? bigPowerPlantCost
+                    : Global.buildingBeingPlaced == "small-business"
+                    ? smallBusinessCost
+                    : Global.buildingBeingPlaced == "big-business"
+                    ? bigBusinessCost
+                    : null;
+
+                if (Input.GetMouseButtonDown(0) && HasResources(cost))
+                {
                     Instantiate(GetPrefabFromKey(Global.buildingBeingPlaced), pos, Quaternion.identity);
                     Destroy(placeholderGameObject);
                     OccupySpace(worldPosMaybe, GetSpaceFromKey(Global.buildingBeingPlaced));
                     Global.buildingsBuilt.Add(Global.buildingBeingPlaced);
+                    if (cost.food > 0)
+                    {
+                        Global.food -= cost.food;
+                    }
+                    if (cost.people > 0)
+                    {
+                        Global.people -= cost.people;
+                    }
+                    if (cost.energy > 0)
+                    {
+                        Global.energy -= cost.energy;
+                    }
+                    if (cost.money > 0)
+                    {
+                        Global.money -= cost.money;
+                    }
                 }
             }
-            else {
+            else
+            {
                 placeholderGameObject.transform.position = new Vector3(-30f, 0.1f, worldPosMaybe.Item2);
             }
         }
--- 02-city/Assets/Scripts/Global.cs
+++ 02-city/Assets/Scripts/Global.cs
@@ -1,18 +1,23 @@
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using TMPro;
 
 public class Global : MonoBehaviour
 {
     public static int food = 0;
-    public static int people = 0;
+    public static int people = 1;
     public static int energy = 0;
     public static int money = 0;
     public static int days = 0;
 
     public static string buildingBeingPlaced = null;
     public static List<string> buildingsBuilt = new List<string>();
+
+    private bool hasWon = false;
+
+    private GameObject winPanelObj;
+    private GameObject winLabelObj;
 
     public void SetBuildingBeingPlaced(string key) {
         buildingBeingPlaced = key;
@@ -26,10 +31,59 @@ public class Global : MonoBehaviour
 
     private void Start()
     {
-        InvokeRepeating("dayTick", 0, 3.0f);
+        InvokeRepeating("dayTick", 0, 5.0f);
+        winPanelObj = GameObject.Find("Win Panel");
+        winLabelObj = GameObject.Find("Win Label");
+        winPanelObj.SetActive(false);
+    }
+
+    private void CalculateResources() {
+        //int totalPeople = 1;
+        buildingsBuilt.ForEach(delegate (string building)
+        {
+            if (building == "small-farm")
+            {
+                food += 2;
+            }
+            if (building == "big-farm")
+            {
+                food += 5;
+            }
+            if (building == "small-house")
+            {
+                people += 2;
+            }
+            if (building == "big-house")
+            {
+                people += 5;
+            }
+            if (building == "small-power-plant") {
+                energy += 2;
+            }
+            if (building == "big-power-plant")
+            {
+                energy += 5;
+            }
+            if (building == "small-business") {
+                money += 2;
+            }
+            if (building == "big-business") {
+                money += 5;
+            }
+        });
+        //people = totalPeople;
     }
 
     private void dayTick() {
-        days++;
+        if (!hasWon) {
+            CalculateResources();
+            days++;
+        }
+        if (money >= 1000)
+        {
+            winLabelObj.GetComponent<TextMeshProUGUI>().text = "You won in " + days + " days!";
+            winPanelObj.SetActive(true);
+            hasWon = true;
+        }
     }
 }
